97043ed50ecef7ad3b6b41e10d902ace5d9cf8e5
OSmol <Olga_Smolyakova@outlook.com>
2025-12-09 19:50:37 +0300

    Add files via upload

---
+++ first_project/src/com/itacademy/les01/start/MyFirst.java
@@ -0,0 +1,10 @@
+package com.itacademy.les01.start;
+
+public class MyFirst {
+
+	public static void main(String[] args) {
+		System.out.println("Hello,\n JC1 group!!!\n\t\tHel\tlo, JC1 group2!!!");
+
+	}
+
+}
